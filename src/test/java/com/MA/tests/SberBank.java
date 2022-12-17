5d7845bf7a0507b994c1d6910e2904835249e2c6
MaxonSQL <maxom7991@gmail.com>
2022-12-17 18:02:37 +0400

    1test

--- src/test/java/com/MA/tests/SberBank.java
+++ src/test/java/com/MA/tests/SberBank.java
@@ -1,7 +1,9 @@
 package com.MA.tests;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SberBank {
@@ -11,13 +13,36 @@ public class SberBank {
     public void startChromeDriver() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.sberbank.ru/ru/person");
+
+
     }
 
     @Test
     public void test() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.sberbank.ru/ru/person");
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement baseMenu = driver.findElement(By.xpath("//a[@aria-label='Страхование' and contains(@role, 'button')]"));
+        baseMenu.click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
+
 
 
 
